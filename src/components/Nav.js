162633d578d7baac49003aefe3ce57da51d53908
Akerele, Tosin James <akerele.james1699@gmail.com>
2021-07-30 09:05:05 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import './Nav'
+import './Nav.css'
 
 function Nav() {
   return (
     <>
       <nav>
         <ul>
-          <li><a href="#">All</a></li>
+          <li><a href="#">All food</a></li>
           <li><a href="#">Breakfast</a></li>
           <li><a href="#">Lunch</a></li>
           <li><a href="#">Shakes</a></li>
